e2d4a898d8319a1c0d1bcc4b7d75953e558ec20c
Hendi Santika <hendisantika@gmail.com>
2021-01-26 06:37:19 +0700

    #9 - Modify S3StorageService

--- src/main/java/com/hendisantika/springbootawss3/service/S3StorageService.java
+++ src/main/java/com/hendisantika/springbootawss3/service/S3StorageService.java
@@ -1,10 +1,19 @@
 package com.hendisantika.springbootawss3.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.hendisantika.springbootawss3.model.S3File;
+import com.hendisantika.springbootawss3.model.S3FileForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,4 +37,42 @@ public class S3StorageService {
 
     @Autowired
     private AmazonS3 amazonS3;
+
+    /**
+     * Resource search
+     *
+     * @param fileName
+     */
+    public S3FileForm search(String fileName) {
+
+        // Screen display form
+        S3FileForm s3FileForm = new S3FileForm();
+
+        Resource[] resources = null;
+        try {
+            // Specify the Ant pattern (s3: //cinpo/**/*test.*) and get the target list.
+            resources = this.resourcePatternResolver.getResources("s3: //" + BUCKET_NAME + "/ ** / *" + fileName + "." +
+                    " *");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<S3File> s3FileList = new ArrayList<S3File>();
+
+        // Get S3Object for each resource.
+        for (Resource resource : resources) {
+            S3Object s3Object = amazonS3.getObject(new GetObjectRequest("S3.Cinpo", resource.getFilename()));
+
+            S3File s3File = new S3File();
+            s3File.setBucketName(s3Object.getBucketName());
+            s3File.setKey(s3Object.getKey());
+            s3File.setContentType(s3Object.getObjectMetadata().getContentType());
+            s3File.setLastModifiedDate(s3Object.getObjectMetadata().getLastModified());
+
+            s3FileList.add(s3File);
+        }
+        s3FileForm.setFileList(s3FileList);
+
+        return s3FileForm;
+    }
 }
