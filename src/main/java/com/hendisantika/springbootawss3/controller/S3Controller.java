d014f2cb387a3a895b074b2dfeee72b37a2f6396
Hendi Santika <hendisantika@gmail.com>
2021-01-26 06:54:24 +0700

    #10 - Add index.html

--- src/main/java/com/hendisantika/springbootawss3/controller/S3Controller.java
+++ src/main/java/com/hendisantika/springbootawss3/controller/S3Controller.java
@@ -1,8 +1,11 @@
 package com.hendisantika.springbootawss3.controller;
 
 import com.hendisantika.springbootawss3.model.S3FileForm;
+import com.hendisantika.springbootawss3.service.S3StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
@@ -18,12 +21,21 @@ import java.io.IOException;
  */
 @Controller
 public class S3Controller {
+
+    @Autowired
+    private S3StorageService s3StorageService;
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
     @PostMapping(value = "/aws/s3/search")
     public String search(Model model, S3FileForm fileForm) throws IOException {
 
-        S3FileForm s3FileForm = storageService.search(fileForm.getFileName());
+        S3FileForm s3FileForm = s3StorageService.search(fileForm.getFileName());
         model.addAttribute("s3FileForm", s3FileForm);
 
-        return "/aws/s3/downloadFromS3";
+        return "index";
     }
 }
