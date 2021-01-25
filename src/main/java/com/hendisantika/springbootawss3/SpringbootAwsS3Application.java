6a9fd9eca1d65cc32aac3821df94faaad33160f3
Hendi Santika <hendisantika@gmail.com>
2021-01-26 06:08:04 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootawss3/SpringbootAwsS3Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootawss3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAwsS3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootawss3/SpringbootAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootawss3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
